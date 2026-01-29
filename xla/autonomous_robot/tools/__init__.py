e3d9d0c7cc3c9c7e5c79159803b31c3033158a7e
Trần Nguyên Bình <trannguyenbinh.shark@gmail.com>
2026-01-30 05:53:34 +0700

    Delete xla directory

--- xla/autonomous_robot/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Tools Package for Autonomous Robot.
-Contains calibration and testing utilities.
-"""
-
-from pathlib import Path
-
-TOOLS_DIR = Path(__file__).parent
--- xla/autonomous_robot/tools/calibration/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Calibration Tools Package.
-"""
-
-from .lane_calibration import LaneCalibrationTool
-from .depth_calibration import DepthCalibrationTool
-from .camera_calibration import CameraCalibrationTool
-
-__all__ = ['LaneCalibrationTool', 'DepthCalibrationTool', 'CameraCalibrationTool']
